76058a87eab7d84dfd2897572caa65347502cf75
ethan-logue <emlrcc@rit.edu>
2023-06-20 16:29:19 -0400

    base react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <p>hello world</p>
+      <p>Hello world</p>
     </div>
   );
 }
