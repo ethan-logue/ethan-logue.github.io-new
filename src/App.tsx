87d8e99893d1129372d78450ff6ffe944d02727c
ethan-logue <emlrcc@rit.edu>
2023-06-21 15:05:34 -0400

    added rotating island

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Island } from "./models/low-poly-island/Island";
+import { Suspense } from "react";
+import * as THREE from "three";
 
 function App() {
+
+  const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 50, 100 );
+
   return (
-    <div>
-      <p>Hello world</p>
+    <div className="app">
+      <Canvas camera={{fov: 18, position: 100}}>
+        <ambientLight intensity={1.25} />
+        <Suspense fallback={null}>
+          <Island />
+        </Suspense>
+        <Environment preset="sunset" />
+        <OrbitControls enableZoom={false} autoRotate={true} autoRotateSpeed={-1} />
+      </Canvas>
     </div>
   );
 }
-
+ 
 export default App;
