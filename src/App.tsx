121c1b46f72462695aae7d4900f7ebdabdeac0f3
ethan-logue <emlrcc@rit.edu>
2023-06-24 17:55:43 -0400

    design now matches figma, dont know how i feel about it

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ const App: React.FC = () => {
       const atSnappingPoint = target.scrollTop % target.offsetHeight === 0;
       const timeOut = atSnappingPoint ? 0 : 150;
 
+      const topCircle = document.querySelector('.top-circle') as HTMLElement;
+      const topOutline = document.querySelector('.top-outline') as HTMLElement;
       const bottomCircle = document.querySelector('.bottom-circle') as HTMLElement;
       const bottomOutline = document.querySelector('.bottom-outline') as HTMLElement;
 
@@ -24,11 +26,25 @@ const App: React.FC = () => {
         if (!timeOut) {
           console.log('Scroller snapped!');
           if (bottomCircle.classList.contains('home-bottom-circle')) {
+            topCircle.classList.remove('home-top-circle');
+            topCircle.classList.add('about-top-circle');
+            topOutline.classList.remove('home-top-outline');
+            topOutline.classList.add('about-top-outline');
+
             bottomCircle.classList.remove('home-bottom-circle');
             bottomCircle.classList.add('about-bottom-circle');
+            bottomOutline.classList.remove('home-bottom-outline');
+            bottomOutline.classList.add('about-bottom-outline');
           } else {
+            topCircle.classList.remove('about-top-circle');
+            topCircle.classList.add('home-top-circle');
+            topOutline.classList.remove('about-top-outline');
+            topOutline.classList.add('home-top-outline');
+
             bottomCircle.classList.remove('about-bottom-circle');
             bottomCircle.classList.add('home-bottom-circle');
+            bottomOutline.classList.remove('about-bottom-outline');
+            bottomOutline.classList.add('home-bottom-outline');
           }
 
         } else {
@@ -43,8 +59,10 @@ const App: React.FC = () => {
 
   return (
     <div className="app">
+      <div className='top-circle home-top-circle'></div>
+      <div className='top-outline home-top-outline'></div>
       <div className='bottom-circle home-bottom-circle'></div>
-      <div className='bottom-outline'></div>
+      <div className='bottom-outline home-bottom-outline'></div>
       <Home />
       <About />
       <Skills />
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -4,8 +4,9 @@ import './About.css';
 const About: React.FC = () => {
     return (
         <div className='page about'>
-            <div className='page-container'>
+            <div className='about-container'>
                 <h1 className='page-title'>About Me</h1>
+                <p className='about-bio'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore accusamus quam error! Maiores quas ipsam corrupti, nisi aut eligendi blanditiis molestias iusto quasi quod officiis ad iste earum animi nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam cumque, itaque, voluptatibus est nemo ipsa ab repellat, officia asperiores debitis sint culpa accusantium repudiandae. Modi consectetur odio vero natus unde?</p>
             </div>
         </div>
     );
