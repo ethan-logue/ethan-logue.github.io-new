7476d3cd1d0d6c1de1e1c91440d1ac6e0f87ab96
Ethan L <ethanlogue03@gmail.com>
2023-09-26 21:43:32 -0400

    fixed projects issue, also did some other tweaks

--- src/pages/projects/ProjectItem.tsx
+++ src/pages/projects/ProjectItem.tsx
@@ -17,7 +17,7 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ name, description, tag, image
 
     const [ref, inView] = useInView({
         triggerOnce: true,
-        threshold: 0.2
+        threshold: 0
     });
 
     const motionProject= {
